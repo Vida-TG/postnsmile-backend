d20fc199c3b27f0f3909e34020ecdcc01e5a2438
Vida-TG <victorademiju.t@gmail.com>
2023-09-17 03:02:51 +0100

    Edits complete

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -42,8 +42,12 @@ module.exports.Login = async (req, res, next) => {
       }
        const token = createSecretToken(user._id);
        res.cookie("token", token, {
-         withCredentials: true,
-         httpOnly: false,
+          domain: "https://postsmile.vercel.app",
+          path: "/",
+          withCredentials: true,
+          httpOnly: false,
+          secure: true,
+          sameSite: "none",
        });
        res.status(201).json({ message: "User logged in successfully", success: true });
        next()
